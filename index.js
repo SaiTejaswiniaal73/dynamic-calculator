8f5824563e25a5c9af922d277df26799be303f78
SaiTejaswiniaal73 <saitejaswini.aal@gmail.com>
2025-01-29 22:14:07 +0530

    buttons added

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// let h1=document.getElementById("dis")
+// function calc(){
+//     h1.innerHTML="hello"
+// }
+
+let display=document.getElementById("display")
+function calc(event){
+    display.value+=event.target.value
+    // console.log(event.target.value)
+}
+function evaluate(event){
+    
+}
